a97d7ad654b18f94bd9345f23df700c85be1803b
Vinze <bismareeo@tradedash.com>
2019-08-21 11:47:14 -0700

    sign up refactor using compose

--- src/components/Authentication/SignUp/index.js
+++ src/components/Authentication/SignUp/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
+import { compose } from 'recompose';
 
 import {FirebaseContext, withFirebase} from '../../../firebase';
 import * as ROUTES from '../../../constants/routes';
@@ -32,6 +33,7 @@ class SignUpFormBase extends Component {
       .doCreateUserWithEmailAndPassword(email, passwordOne)
       .then(authUser => {
         this.setState({ ...INITIAL_STATE });
+        this.props.history.push(ROUTES.HOME);
       })
       .catch(error => {
         this.setState({ error });
@@ -95,7 +97,7 @@ const SignUpLink = () => (
   </p>
 );
 
-const SignUpForm = withFirebase(SignUpFormBase);
+const SignUpForm = compose(withRouter,withFirebase)(SignUpFormBase);
 
 export default SignUpPage;
 
