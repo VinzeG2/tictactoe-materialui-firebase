cfb3ebead91a0f896edd5dce52abe8988c3fa2ae
Vinze <bismareeo@tradedash.com>
2019-09-12 11:08:07 -0700

    useless imports deleted

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 
 import * as ROUTES from '../../constants/routes'
 import { AuthUserContext } from '../Authentication/Session';
@@ -7,9 +6,7 @@ import { AuthUserContext } from '../Authentication/Session';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
 
 
 const useStyles = makeStyles(theme => ({
